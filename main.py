492476ee13a66286e703c9b8f5213fa67792639d
mateus Oliveira hypeone <mateus.osilva@hypeone.com>
2022-11-08 09:20:46 -0300

    vectorize context Explore data

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pandas as pd
-from models import LSTM
 
 dataset = pd.read_csv("uci_base_machine_learning.csv")
 
-dataset.drop('Air temperature [K]',
-             'Process temperature [K]', 'Rotational speed [rpm]', 'Torque [Nm]',
-             'Tool wear [min]', 'Machine failure')
-# lstm_classifier = LSTM()
+dataset.keys()
 
+x_dataset = dataset.drop(columns=['UDI', 'Product ID', 'Type','Tool wear [min]',
+                                  'Machine failure', 'TWF', 'HDF', 'PWF', 'OSF',
+                                  'RNF'])
 
-#Datasets and DataLoaders
+y_dataset = dataset.drop(columns=['UDI', 'Product ID', 'Type', 'Air temperature [K]',
+                               'Process temperature [K]', 'Rotational speed [rpm]', 'Torque [Nm]',
+                               'Tool wear [min]', 'Machine failure'])
+
+channels = list(x_dataset.keys())
+
+sequence = 30
+
+import numpy as np
+
+dict_features_x = {}
+#dict_keys(['Air temperature [K]', 'Process temperature [K]', 'Rotational speed [rpm]', 'Torque [Nm]'])
+matrix_channels = np.array([np.array((x_dataset[feature].tolist())) for feature in x_dataset.keys()])
+
+context = 5
+
