4b0b79357ed07d19f53cd4116e448e1f5363015a
Mateus Oliveira <m203656@dac.unicamp.br>
2023-04-11 20:19:55 -0300

    code low frequency and VAR algorithm

--- classical_methods_forecast/view_classical_methods.py
+++ classical_methods_forecast/view_classical_methods.py
@@ -1,34 +1,130 @@
-# from filters.filter_avoid_amplitude import FiltersAvoidLowFreq
+from filters.filter_avoid_amplitude import FiltersAvoidLowFreq
+from statsmodels.tsa.vector_ar.var_model import VAR
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    import numpy as np
-    from scipy.signal import butter, filtfilt
 
-    # create a sample 1D signal
-    fs = 92554  # sampling frequency
-    t = np.linspace(0, 1, fs, endpoint=False)
-    # x = np.sin(2 * np.pi * 50 * t) + np.sin(2 * np.pi * 120 * t)
+def view_filter_low_pass():
+    # plot the original and filtered signals
 
     payload_hex = "../Datasets/dataset_TPV_sensors/hex/payloadHex.csv"
 
     df = pd.read_csv(payload_hex)
 
-    # define the default filter parameters
-    order = 1
-    cutoff_freq = 0.01 * fs / 2  # Hz
-    nyquist_freq = 0.5 * fs
-    normal_cutoff_freq = cutoff_freq / nyquist_freq
-    b, a = butter(order, normal_cutoff_freq, btype='low', analog=False)
+    feature = "InletPressure"
 
-    # apply zero-phase filtering
-    y = filtfilt(b, a, df["InverterSpeed"].tolist())
+    x = df[feature].tolist()
 
-    # plot the original and filtered signals
-    import matplotlib.pyplot as plt
+    fs = len(x)  # sampling frequency
+    filters_avoid_low_freq = FiltersAvoidLowFreq(fs, value_cutoff_freq=0.05)
+
+    y = filters_avoid_low_freq.apply(x)
+
+    t = np.linspace(0, 1, fs, endpoint=False)
 
     plt.figure()
-    plt.plot(t, df["InverterSpeed"].tolist(), 'b', alpha=0.5)
+    plt.plot(t, x, 'b', alpha=0.5)
     plt.plot(t, y, 'r')
     plt.legend(['Original', 'Filtered'])
+    plt.savefig(f'savefig/{feature}.png')
     plt.show()
+
+
+def vector_autoRegressive_model():
+    payload_hex = "../Datasets/dataset_TPV_sensors/hex/payloadHex.csv"
+
+    df = pd.read_csv(payload_hex)
+
+    split_test = 45000
+
+    df = df.drop(labels=["Time"], axis=1)
+
+    inlet_pressure = df["InletPressure"].tolist()
+
+    fs = len(inlet_pressure)
+
+    filters_avoid_low_freq = FiltersAvoidLowFreq(fs, value_cutoff_freq=0.05)
+    df = filters_avoid_low_freq.filter_multiple_sensor(df, "InletPressure",
+                                                       "OutletPressure",
+                                                       "OutletTemperature",
+                                                       "InverterSpeed")
+
+    train = df[:-split_test]
+    test = df[-split_test:]
+
+    for i in range(20):
+        model = VAR(train)
+        results = model.fit(i)
+        print('Order =', i)
+        print('AIC: ', results.aic)
+        print('BIC: ', results.bic)
+        print()
+
+    window = 500
+    result = model.fit(window)
+    # print(result.summary())
+
+    lagged_Values = train.values[-window:]
+    pred = result.forecast(y=lagged_Values, steps=split_test)
+
+    # idx = pd.date_range('2015-01-01', periods=split_test, freq='MS')
+    df_forecast = pd.DataFrame(data=pred, columns=['InletPressure_forecast',
+                                                   'OutletPressure_forecast',
+                                                   'OutletTemperature_forecast',
+                                                   'InverterSpeed_forecast'])
+
+    test_original = df[:-split_test]
+    test_original.index = pd.to_datetime(test_original.index)
+
+    # 'InletPressure',
+    # 'OutletPressure',
+    # 'OutletTemperature',
+    # 'InverterSpeed'
+
+    feature = 'InletPressure'
+
+    print((test_original[feature].shape,
+           df_forecast[f'{feature}_forecast'].shape))
+
+    plt.figure()
+    plt.plot(test_original[feature].tolist())
+    plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.savefig(f'savefig_VAR/{feature}.png')
+
+    feature = 'OutletPressure'
+
+    print((test_original[feature].shape,
+           df_forecast[f'{feature}_forecast'].shape))
+
+    plt.figure()
+    plt.plot(test_original[feature].tolist())
+    plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.savefig(f'savefig_VAR/{feature}.png')
+
+    feature = 'OutletTemperature'
+
+    print((test_original[feature].shape,
+           df_forecast[f'{feature}_forecast'].shape))
+
+    plt.figure()
+    plt.plot(test_original[feature].tolist())
+    plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.savefig(f'savefig_VAR/{feature}.png')
+
+    feature = 'InverterSpeed'
+
+    print((test_original[feature].shape,
+           df_forecast[f'{feature}_forecast'].shape))
+
+    plt.figure()
+    plt.plot(test_original[feature].tolist())
+    plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.savefig(f'savefig_VAR/{feature}.png')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    # view_filter_low_pass()
+    vector_autoRegressive_model()
--- filters/filter_avoid_amplitude.py
+++ filters/filter_avoid_amplitude.py
@@ -1,6 +1,30 @@
 import numpy as np
 from scipy.signal import butter, filtfilt
+import pandas as pd
+
 
 #
-# class FiltersAvoidLowFreq:
-#     def __init__(self):
+class FiltersAvoidLowFreq:
+    def __init__(self, points, value_cutoff_freq=0.01, order=1):
+        self.fs = points
+        self.cutoff_freq = value_cutoff_freq * self.fs / 2
+        self.nyquist_freq =  0.5 * self.fs
+        self.normal_cutoff_freq = self.cutoff_freq / self.nyquist_freq
+        self.order = order
+
+    def apply(self, signal):
+        b, a = butter(self.order, self.normal_cutoff_freq, btype='low', analog=False)
+        # apply zero-phase filtering
+        return filtfilt(b, a, signal)
+
+    def filter_multiple_sensor(self, df_signal, *signals_keys):
+        dict_of_signals = {}
+
+        for signal_key in signals_keys:
+            x = df_signal[signal_key].tolist()
+            x_signal_filter = self.apply(x)
+
+            dict_of_signals[signal_key] = x_signal_filter
+
+        return pd.DataFrame(dict_of_signals)
+
