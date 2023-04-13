f719f7f3c25e9498f9d095555ee75634f665ab28
Mateus Oliveira <m203656@dac.unicamp.br>
2023-04-13 13:56:54 -0300

    new images in classical methods

--- classical_methods_forecast/view_classical_methods.py
+++ classical_methods_forecast/view_classical_methods.py
@@ -41,7 +41,8 @@ def vector_autoRegressive_model():
 
     df = pd.read_csv(payload_hex)
 
-    split_test = 45000
+    split_test = 5000
+    split_test_end = 16000
 
     df = df.drop(labels=["Time"], axis=1)
 
@@ -49,14 +50,17 @@ def vector_autoRegressive_model():
 
     fs = len(inlet_pressure)
 
-    filters_avoid_low_freq = FiltersAvoidLowFreq(fs, value_cutoff_freq=0.05)
-    df = filters_avoid_low_freq.filter_multiple_sensor(df, "InletPressure",
-                                                       "OutletPressure",
-                                                       "OutletTemperature",
-                                                       "InverterSpeed")
+    # filters_avoid_low_freq = FiltersAvoidLowFreq(fs, value_cutoff_freq=0.05)
 
-    train = df[:-split_test]
-    test = df[-split_test:]
+    df_old = df
+
+    # df = filters_avoid_low_freq.filter_multiple_sensor(df, "InletPressure",
+    #                                                    "OutletPressure",
+    #                                                    "OutletTemperature",
+    #                                                    "InverterSpeed")
+
+    train = df[:split_test]
+    test = df[split_test:split_test_end]
 
     for i in range(20):
         model = VAR(train)
@@ -71,7 +75,7 @@ def vector_autoRegressive_model():
     # print(result.summary())
 
     lagged_Values = train.values[-window:]
-    pred = result.forecast(y=lagged_Values, steps=split_test)
+    pred = result.forecast(y=lagged_Values, steps=split_test_end - split_test)
 
     # idx = pd.date_range('2015-01-01', periods=split_test, freq='MS')
     df_forecast = pd.DataFrame(data=pred, columns=['InletPressure_forecast',
@@ -79,56 +83,60 @@ def vector_autoRegressive_model():
                                                    'OutletTemperature_forecast',
                                                    'InverterSpeed_forecast'])
 
-    test_original = df[:-split_test]
-    test_original.index = pd.to_datetime(test_original.index)
+    #test_original = df[:-split_test]
+    test.index = pd.to_datetime(test.index)
     # 'InletPressure',
     # 'OutletPressure',
     # 'OutletTemperature',
     # 'InverterSpeed'
 
-    feature = 'OutletTemperature'
+    feature = 'InletPressure'
 
-    print((test_original[feature].shape,
+    print((test[feature].shape,
            df_forecast[f'{feature}_forecast'].shape))
 
     plt.figure()
-    plt.plot(test_original[feature].tolist())
+    plt.plot(test[feature].tolist())
     plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.title(f"{feature}")
     plt.savefig(f'savefig_VAR/{feature}.png')
 
     feature = 'OutletPressure'
 
-    print((test_original[feature].shape,
+    print((test[feature].shape,
            df_forecast[f'{feature}_forecast'].shape))
 
     plt.figure()
-    plt.plot(test_original[feature].tolist())
+    plt.plot(test[feature].tolist())
     plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.title(f"{feature}")
     plt.savefig(f'savefig_VAR/{feature}.png')
 
     feature = 'OutletTemperature'
 
-    print((test_original[feature].shape,
+    print((test[feature].shape,
            df_forecast[f'{feature}_forecast'].shape))
 
     plt.figure()
-    plt.plot(test_original[feature].tolist())
+    plt.plot(test[feature].tolist())
     plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.title(f"{feature}")
     plt.savefig(f'savefig_VAR/{feature}.png')
 
     feature = 'InverterSpeed'
 
-    print((test_original[feature].shape,
+    print((test[feature].shape,
            df_forecast[f'{feature}_forecast'].shape))
 
     plt.figure()
-    plt.plot(test_original[feature].tolist())
+    plt.plot(test[feature].tolist())
     plt.plot(df_forecast[f'{feature}_forecast'].tolist())
+    plt.title(f"{feature}")
     plt.savefig(f'savefig_VAR/{feature}.png')
 
     plt.show()
 
 
 if __name__ == '__main__':
-    view_filter_low_pass()
-    # vector_autoRegressive_model()
+    # view_filter_low_pass()
+    vector_autoRegressive_model()
