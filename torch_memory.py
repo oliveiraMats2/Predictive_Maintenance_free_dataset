7826dcb0dce44a35e573107ecd2a432d1ede94b9
Mateus Oliveira <m203656@dac.unicamp.br>
2023-06-14 09:41:59 -0300

    inference model

--- torch_memory.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
-print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
-print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
