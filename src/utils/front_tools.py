63cc586ceb7feb0a355bc0feaa6cbc8d20d2e0ac
gustavo.torres <gustavomtorres2009@hotmail.com>
2023-07-24 06:45:01 -0400

    Fix json to new pattern

--- src/utils/front_tools.py
+++ src/utils/front_tools.py
@@ -2,40 +2,58 @@ import pandas as pd
 import json
 import numpy as np
 
-def generate_json(property_list, df_true_list, df_pred_list):
-    # Read dataframes
-    properties = []
-
-    for idx, property in enumerate(property_list):
-        
-        current_data = []
-        prevision_data = []
-        df_true_list = np.array(df_true_list)
-        df_pred_list = np.array(df_pred_list)
-
-        for df in df_true_list[idx, :, :]:
-            timestamp = df[1]   # Get timestamp
-            value = df[2]       # Get real value
-            current_data.append({'timestamp': timestamp, 'value': value})
-
-        for df in df_pred_list[idx, :, :]:
-            timestamp = df[1]   # Get timestamp
-            value = df[4]       # Get prediction yhat value
-            prevision_data.append({'timestamp': timestamp, 'value': value})
-        
-        content = {
-            "property":property,
-            "value":100,
-            "current_data": current_data,
-            "prevision_data": prevision_data
-        }
-        properties.append(content)
+def generate_json_current_anomaly(name_model, feature_name, detect_time, anomaly_type, detection_timestamp, detection, df_current):
+    # Process current and prevision dataframes
+    current_timestamp = list(np.array(df_current['ds']))
+    current_values = list(np.array(df_current['y']))
 
     # Define the JSON header and properties
     data = {
-        "equipment_id": "648a15197e30d0e3725d9a6b",
-        "origin_field": "predictive",
-        "properties": properties
+        "Name_model": name_model,
+        "Feature_name": feature_name,
+        "Detect_time": detect_time,
+        "Anomaly_type": anomaly_type,
+        "Input_vector": {
+            "Timestamp": current_timestamp,
+            "Window_input": current_values
+        },
+        "Detection": {
+            "Timestamp": detection_timestamp,
+            "Detection": detection
+        }
+    }
+
+    # Convert the JSON data to a string
+    json_data = json.dumps(data)
+
+    return json_data
+
+def generate_json_future_anomaly(name_model, feature_name, detect_time, anomaly_type, detection_timestamp, detection, df_current, df_prevision):
+    # Process current and prevision dataframes
+    current_timestamp = list(np.array(df_current['ds']))
+    current_values = list(np.array(df_current['y']))
+
+    prevision_timestamp = list(np.array(df_prevision['ds']))
+    prevision_values = list(np.array(df_prevision['yhat']))
+
+    # Define the JSON header and properties
+    data = {
+        "Name_model": name_model,
+        "Feature_name": feature_name,
+        "Detect_time": detect_time,
+        "Anomaly_type": anomaly_type,
+        "Input_vector": {
+            "Timestamp": current_timestamp,
+            "Window_input": current_values
+        },
+        "Prevision": {
+            "Timestamp": prevision_timestamp,
+            "Prevision": prevision_values
+        },
+        "Detection": {
+            "Timestamp": detection_timestamp,
+            "Detection": detection
+        }
     }
 
     # Convert the JSON data to a string
@@ -46,23 +64,26 @@ def generate_json(property_list, df_true_list, df_pred_list):
 if __name__ == '__main__':
     # Test json generation script
 
-    # Load 
-    property_list = ['Temperature.InletTemperature', 'Temperature.OutletTemperature', 'Pressure.InletPressure']
+    # Load
+    # name_model, feature_name, detect_time, anomaly_type, detection_timestamp, detection, df_current, df_prevision
+    name_model = 'modelo1'
+    feature_name = 'Temperature.InletTemperature'
     df_true = pd.read_csv('/home/gustavo/Documents/WileC/Predictive_Maintenance_free_dataset/src/utils/df_train.csv')
     df_pred = pd.read_csv('/home/gustavo/Documents/WileC/Predictive_Maintenance_free_dataset/src/utils/df_pred.csv')
-
-    df_pred_list = [df_pred, df_pred, df_pred]
-    df_true_list = [df_true, df_true, df_true]
-
-    json_data = generate_json(property_list=property_list, df_true_list=df_true_list, df_pred_list=df_pred_list)
+    detection_timestamp = df_true['ds'][500]
+    json_data_current = generate_json_current_anomaly(name_model, feature_name, 'current', 'severe', detection_timestamp, 1, df_true)
+    json_data_future = generate_json_future_anomaly(name_model, feature_name, 'current', 'severe', detection_timestamp, 1, df_true, df_pred)
 
     # Save JSON file
-    with open('json_data_v2.json', 'w') as f:
-        json.dump(json_data, f)
+    with open('json_data_current.json', 'w') as f:
+        json.dump(json_data_current, f)
+
+    with open('json_data_future.json', 'w') as f:
+        json.dump(json_data_future, f)
 
     # Read JSON file
     # with open('json_data.json', 'r') as data_file:
     #     data_loaded = json.load(data_file)
 
     # print(json_data == data_loaded)
-    print(json_data)
+    # print(json_data)
