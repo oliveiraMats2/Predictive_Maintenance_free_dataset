837e1db10a87aabc3e972ee453bc23c86101926f
Antonio Pereira <antonio.tech@yahoo.com>
2023-07-25 15:35:37 -0400

    front_tools up

--- src/utils/front_tools.py
+++ src/utils/front_tools.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import numpy as np
 
+
 def generate_json_current_anomaly(name_model, feature_name, detect_time, anomaly_type, detection_timestamp, detection, df_current):
     # Process current and prevision dataframes
     current_timestamp = list(np.array(df_current['ds']))
@@ -46,6 +47,7 @@ def generate_json_current_anomaly(name_model, feature_name, detect_time, anomaly
 
     return json_data
 
+
 def generate_json_future_anomaly(name_model, feature_name, detect_time, anomaly_type, detection_timestamp, detection, df_current, df_prevision):
     # Process current and prevision dataframes
     current_timestamp = list(np.array(df_current['ds']))
@@ -74,35 +76,36 @@ def generate_json_future_anomaly(name_model, feature_name, detect_time, anomaly_
     data = {
         "equipment_id": "648a15197e30d0e3725d9a6b",
         "origin_field": "predictive",
-        "properties": {     # De acordo com o padrão do Jean
-            "property": feature_name,
-            "current_data": current_data,
-            "prevision_data": prevision_data
-        },
-        "messages": {       # De acordo com PDF
-            "Name_model": name_model,
-            "Feature_name": feature_name,
-            "Detect_time": detect_time,
-            "Anomaly_type": anomaly_type,
-            "Input_vector": {
-                "Timestamp": current_timestamp,
-                "Window_input": current_values
+        "evaluation_criticality": True,
+        "properties": [
+            {     # De acordo com o padrão do Jean
+                "property": feature_name,
+                "value": 8,
+                "current_data": current_data,
+                "prevision_data": prevision_data
             },
-            "Prevision": {
-                "Timestamp": prevision_timestamp,
-                "Prevision": prevision_values
-            },
-            "Detection": {
-                "Timestamp": detection_timestamp,
-                "Detection": detection
-            }
-        }
+        ],
+        #"messages": {       # De acordo com PDF
+        #    "Name_model": name_model,
+        #    "Feature_name": feature_name,
+        #    "Detect_time": detect_time,
+        #    "Anomaly_type": anomaly_type,
+        #    "Prevision": {
+        #        "Timestamp": prevision_timestamp,
+        #        "Prevision": prevision_values
+        #    },
+        #    "Detection": {
+        #        "Timestamp": detection_timestamp,
+        #        "Detection": detection
+        #    }
+        #}
     }
 
     # Convert the JSON data to a string
     json_data = json.dumps(data)
 
     return json_data
+
 
 if __name__ == '__main__':
     # Test json generation script
@@ -111,8 +114,8 @@ if __name__ == '__main__':
     # name_model, feature_name, detect_time, anomaly_type, detection_timestamp, detection, df_current, df_prevision
     name_model = 'modelo1'
     feature_name = 'Temperature.InletTemperature'
-    df_true = pd.read_csv('/home/gustavo/Documents/WileC/Predictive_Maintenance_free_dataset/src/utils/df_train.csv')
-    df_pred = pd.read_csv('/home/gustavo/Documents/WileC/Predictive_Maintenance_free_dataset/src/utils/df_pred.csv')
+    df_true = pd.read_csv('/media/antonio/AllData/Workspace/git/general/ufam/Predictive_Maintenance_free_dataset/src/utils/df_train.csv')
+    df_pred = pd.read_csv('/media/antonio/AllData/Workspace/git/general/ufam/Predictive_Maintenance_free_dataset/src/utils/df_pred.csv')
     detection_timestamp = df_true['ds'][500]        # Choose timestamp for test
     json_data_current = generate_json_current_anomaly(name_model, feature_name, 'current', 'severe', detection_timestamp, 1, df_true)
     json_data_future = generate_json_future_anomaly(name_model, feature_name, 'future', 'severe', detection_timestamp, 1, df_true, df_pred)
